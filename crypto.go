16362a88b7b12207bba79082921225fcf5516e9b
aceuna <steven.tschudin98@gmail.com>
2024-03-23 13:43:54 +0100

    Last modifications

--- crypto.go
+++ crypto.go
@@ -28,55 +28,58 @@ func createMD5Hash(data string) string {
 }
 
 func deriveKey(password string) []byte {
+	// Generate a key using PBKDF2
 	return pbkdf2.Key([]byte(password), nil, 10000, 32, sha256.New)
 }
 
 func encrypt(data, key []byte) (string, error) {
+	// Create a new cipher block from the key
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return "", err
 	}
-
+	// Create a new GCM block
 	gcm, err := cipher.NewGCM(block)
 	if err != nil {
 		return "", err
 	}
-
+	// Create a nonce
 	nonce := make([]byte, gcm.NonceSize())
 	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
 		return "", err
 	}
-
+	// Encrypt the data using the GCM block and the nonce
 	ciphertext := gcm.Seal(nonce, nonce, data, nil)
 	return base64.StdEncoding.EncodeToString(ciphertext), nil
 }
 
 func decrypt(data string, key []byte) (string, error) {
+	// Decode the data from base64
 	ciphertext, err := base64.StdEncoding.DecodeString(data)
 	if err != nil {
 		return "", err
 	}
-
+	// Create a new cipher block from the key
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return "", err
 	}
-
+	// Create a new GCM block
 	gcm, err := cipher.NewGCM(block)
 	if err != nil {
 		return "", err
 	}
-
+	// Get the nonce size
 	nonceSize := gcm.NonceSize()
 	if len(ciphertext) < nonceSize {
 		return "", fmt.Errorf("ciphertext too short")
 	}
-
+	// Get the nonce and the ciphertext from the data
 	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
 	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
 	if err != nil {
 		return "", err
 	}
-
+	// Return the decrypted data
 	return string(plaintext), nil
 }
